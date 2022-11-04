5aaea629d2f447f16736b5e8367c4d6d48b39064
juan <juan1316diossantiago@gmail.com>
2022-11-03 22:17:43 -0500

    logros

--- src/component/Estudiantes/Logros/LogrosEstudiante.jsx
+++ src/component/Estudiantes/Logros/LogrosEstudiante.jsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import image1 from '../../../style/img/logros/1.PNG';
+import image2 from '../../../style/img/logros/1.1.PNG';
+import image3 from '../../../style/img/logros/2.PNG';
+import image4 from '../../../style/img/logros/2.2.PNG';
+import image5 from '../../../style/img/logros/3.PNG';
+import image6 from '../../../style/img/logros/3.3.PNG';
+import image7 from '../../../style/img/logros/4.PNG';
+import image8 from '../../../style/img/logros/4.4.PNG';
+import image9 from '../../../style/img/logros/5.PNG';
+import image10 from '../../../style/img/logros/5.5.PNG';
+import image11 from '../../../style/img/logros/6.PNG';
+import image12 from '../../../style/img/logros/6.6.PNG';
+import image13 from '../../../style/img/logros/7.PNG';
+import image14 from '../../../style/img/logros/7.7.PNG';
+import image15 from '../../../style/img/logros/8.PNG';
+import image16 from '../../../style/img/logros/8.8.PNG';
+
 
 const LogrosEstudiante = () => {
   return (
@@ -11,55 +28,97 @@ const LogrosEstudiante = () => {
         </div>
         <div className='d-flex flex-wrap justify-content-around m-2'>
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image1} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
 
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image2}  alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
 
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image3} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
 
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image4} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
 
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image5} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image6} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image7} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
             <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src={image9} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image10} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image11} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image12} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image13} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image14} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image15} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src={image16} alt="Card image cap" />
+                <div className="card-body">
+                    <p className="card-text">Logro</p>
                 </div>
             </div>
         </div>
