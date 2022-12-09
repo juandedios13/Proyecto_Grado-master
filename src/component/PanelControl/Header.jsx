59daa3d2753440e1d69f502a4bb767930812db58
juan <juan1316diossantiago@gmail.com>
2022-12-08 20:38:58 -0500

    prueba

--- src/component/PanelControl/Header.jsx
+++ src/component/PanelControl/Header.jsx
@@ -11,6 +11,16 @@ const Header = () => {
             <i className="fas fa-bars" />
           </a>
         </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          <a href="" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          <a href="" className="nav-link">
+            Contact
+          </a>
+        </li>
       </ul>
       {/* Right navbar links */}
       <ul className="navbar-nav ml-auto">
